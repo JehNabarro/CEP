f3140e74a073ec64a4eebc395c7ae82cde2cdbf7
jessica nabarro <jeh.nabarro@gmail.com>
2018-06-15 14:24:16 -0300

    CEP e Mask

--- app/src/main/java/com/example/android/cep/MainActivity.java
+++ app/src/main/java/com/example/android/cep/MainActivity.java
@@ -1,40 +1,43 @@
 package com.example.android.cep;
 
-import android.app.LoaderManager;
-import android.content.Context;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
 
-import java.io.BufferedReader;
+import org.apache.http.util.EntityUtils;
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.nio.charset.Charset;
-import java.text.SimpleDateFormat;
+import java.io.StringReader;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
 public class MainActivity extends AppCompatActivity {
 
-    /** Tag for the log messages */
+    /**
+     * Tag for the log messages
+     */
     public static final String LOG_TAG = MainActivity.class.getSimpleName();
-
-    /** URL to query the USGS dataset for earthquake information */
-    private static final String USGS_REQUEST_URL =
-            "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=2014-12-01&minmagnitude=7";
+    EditText cep;
+    TextView preco;
+    TextView prazo;
+    Button calc;
+    Button comprar;
 
 
     @Override
@@ -42,173 +45,111 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        cep = (EditText) findViewById(R.id.cepEdit);
+        preco = (TextView) findViewById(R.id.precoTexto);
+        prazo = (TextView) findViewById(R.id.prazoTexto);
+        comprar = (Button) findViewById(R.id.pagarButton);
+        calc = (Button) findViewById(R.id.calcButton);
 
-        Button calcularFrete = (Button) findViewById(R.id.calcButton);
-        calcularFrete.setOnClickListener(new View.OnClickListener() {
+        cep.addTextChangedListener(MaskEditUtil.mask(cep, MaskEditUtil.FORMAT_CEP));
+
+
+        calc.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                // Kick off an {@link AsyncTask} to perform the network request
-                TsunamiAsyncTask task = new TsunamiAsyncTask();
-                task.execute();
+            public void onClick(View v) {
+
+                // por ser uma classe tem que ser criado o objeto,
+                new XmlAsyncTask().execute(cep.getText().toString());
+
+
             }
         });
 
 
+        comprar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(MainActivity.this,"PAGAR", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
     }
 
-    /**
-     * Update the screen to display information from the given {@link Event}.
-     */
-    private void updateUi() {
+    public static Document loadXMLFromString(String xml)
+    {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = null;
+        try {
+            builder = factory.newDocumentBuilder();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        }
+        InputSource is = new InputSource(new StringReader(xml));
+        try {
+            return builder.parse(is);
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-        Log.e(LOG_TAG, "UPI ATUALIZADA!");
-
+        return null;
     }
 
-    /**
-     * {@link AsyncTask} to perform the network request on a background thread, and then
-     * update the UI with the first earthquake in the response.
-     */
-    private class TsunamiAsyncTask extends AsyncTask<URL, Void, Event> {
+
+    class XmlAsyncTask extends AsyncTask<String, Void, String> {
+
 
         @Override
-        protected Event doInBackground(URL... urls) {
-            // Create URL object
-            URL url = createUrl(USGS_REQUEST_URL);
+        protected void onPreExecute() {
+            super.onPreExecute();
 
-            // Perform HTTP request to the URL and receive a JSON response back
-            String jsonResponse = "";
-            try {
-                jsonResponse = makeHttpRequest(url);
-            } catch (IOException e) {
-                // TODO Handle the IOException
-            }
-
-            // Extract relevant fields from the JSON response and create an {@link Event} object
-            Event earthquake = extractFeatureFromJson(jsonResponse);
-
-            // Return the {@link Event} object as the result fo the {@link TsunamiAsyncTask}
-            return earthquake;
         }
 
-        /**
-         * Update the screen with the given earthquake (which was the result of the
-         * {@link TsunamiAsyncTask}).
-         */
+
+        // Metodo responsavel por fazer a requisição
+
         @Override
-        protected void onPostExecute(Event earthquake) {
-            if (earthquake == null) {
-                return;
-            }
-
-            updateUi();
-        }
-
-        /**
-         * Returns new URL object from the given string URL.
-         */
-        private URL createUrl(String stringUrl) {
-            URL url = null;
+        protected String doInBackground(String... urls) {
             try {
-                url = new URL(stringUrl);
-            } catch (MalformedURLException exception) {
-                Log.e(LOG_TAG, "Error with creating URL", exception);
-                return null;
-            }
-            return url;
-        }
 
-        /**
-         * Make an HTTP request to the given URL and return a String as the response.
-         */
-        private String makeHttpRequest(URL url) throws IOException {
-            String jsonResponse = "";
+                //------------------>>
+                HttpGet httppost = new HttpGet("http://ws.correios.com.br/calculador/CalcPrecoPrazo.aspx?nCdEmpresa=08082650&sDsSenha=564321&sCepOrigem=70002900&sCepDestino=" + urls[0] + "&nVlPeso=1&nCdFormato=1&nVlComprimento=20&nVlAltura=20&nVlLargura=20&sCdMaoPropria=n&nVlValorDeclarado=0&sCdAvisoRecebimento=n&nCdServico=04510&nVlDiametro=0&StrRetorno=xml&nIndicaCalculo=3");
+                HttpClient httpclient = new DefaultHttpClient();
+                HttpResponse response = httpclient.execute(httppost);
 
-            if (url == null){
-                return  jsonResponse;
-            }
+                // StatusLine stat = response.getStatusLine();
+                int status = response.getStatusLine().getStatusCode();
 
-            HttpURLConnection urlConnection = null;
-            InputStream inputStream = null;
-            try {
-                urlConnection = (HttpURLConnection) url.openConnection();
-                urlConnection.setRequestMethod("GET");
-                urlConnection.setReadTimeout(10000 /* milliseconds */);
-                urlConnection.setConnectTimeout(15000 /* milliseconds */);
-                urlConnection.connect();
+                if (status == 200) {
+                    HttpEntity entity = response.getEntity();
+                    String data = EntityUtils.toString(entity);
 
-                // Caso
-                if (urlConnection.getResponseCode() == 200){
-                    inputStream = urlConnection.getInputStream();
-                    jsonResponse = readFromStream(inputStream);
-                }else {
-                    Log.e(LOG_TAG, "Error response code: " + urlConnection.getResponseCode());
+                    return data;
                 }
+
 
             } catch (IOException e) {
-                Log.e(LOG_TAG, "Problem retrieving the earthquake JSON results.", e);
-            } finally {
-                if (urlConnection != null) {
-                    urlConnection.disconnect();
-                }
-                if (inputStream != null) {
-                    // function must handle java.io.IOException here
-                    inputStream.close();
-                }
+                e.printStackTrace();
             }
-            return jsonResponse;
+            return "";
         }
 
-        /**
-         * Convert the {@link InputStream} into a String which contains the
-         * whole JSON response from the server.
-         */
-        private String readFromStream(InputStream inputStream) throws IOException {
-            StringBuilder output = new StringBuilder();
-            if (inputStream != null) {
-                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
-                BufferedReader reader = new BufferedReader(inputStreamReader);
-                String line = reader.readLine();
-                while (line != null) {
-                    output.append(line);
-                    line = reader.readLine();
-                }
-            }
-            return output.toString();
+        protected void onPostExecute(String result) {
+
+
+            Document doc = loadXMLFromString(result);
+
+            String valor = doc.getElementsByTagName("Valor").item(0).getTextContent();
+            String prazoEntrega = doc.getElementsByTagName("PrazoEntrega").item(0).getTextContent();
+
+            preco.setText(valor);
+            prazo.setText(prazoEntrega);
+
         }
 
-        /**
-         * Return an {@link Event} object by parsing out information
-         * about the first earthquake from the input earthquakeJSON string.
-         */
-        private Event extractFeatureFromJson(String earthquakeJSON) {
 
-            if (TextUtils.isEmpty(earthquakeJSON)){
-                return null;
-            }
-
-
-            try {
-                JSONObject baseJsonResponse = new JSONObject(earthquakeJSON);
-                JSONArray featureArray = baseJsonResponse.getJSONArray("features");
-
-                // If there are results in the features array
-                if (featureArray.length() > 0) {
-                    // Extract out the first feature (which is an earthquake)
-                    JSONObject firstFeature = featureArray.getJSONObject(0);
-                    JSONObject properties = firstFeature.getJSONObject("properties");
-
-                    // Extract out the title, time, and tsunami values
-                    String title = properties.getString("title");
-                    int time = properties.getInt("time");
-
-                    // Create a new {@link Event} object
-                    return new Event(title, time);
-                }
-            } catch (JSONException e) {
-                Log.e(LOG_TAG, "Problem parsing the earthquake JSON results", e);
-            }
-            return null;
-        }
     }
 }
